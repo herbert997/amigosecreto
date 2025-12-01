92e79e18e8d34efe8c94f9886f4d0b771676ce74
herbert997 <146259668+herbert997@users.noreply.github.com>
2025-12-01 16:52:53 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// config.php
+return [
+    'db_host' => '127.0.0.1',
+    'db_name' => 'amigo_secreto',
+    'db_user' => 'root',
+    'db_pass' => '',
+    'base_url' => 'http://localhost/amigosecreto' // Ex.: https://seusite.com/amigosecreto (sem barra final)
+];
